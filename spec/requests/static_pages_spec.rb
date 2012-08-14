8663107b5d379bcfb44f1d7982436a0408725294
safamj <safalmj@gmail.com>
2012-08-15 00:33:00 +0545

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,17 +3,32 @@ require 'spec_helper'
 describe "StaticPages" do
 
   describe "Home page" do
-    it "should have the content 'Sample App'" do
+    it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      #page.should have_content('Sample App')
+      page.should have_selector('h1',
+                        :text => 'Sample App')
+    end
+
+    it "should have title page 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => 'Ruby on Rails Tutorial Sample App | Home')
     end
   end
 
   describe "Help page" do
-  	it "should have the content 'Help'" do
+  	it "should have the h1 'Help'" do
   	  visit '/static_pages/help'
-  	  page.should have_content('Help')
+  	  page.should have_selector('h1',
+                         :text => 'Help')
   	end
+
+    it "should have title 'Help'" do
+     visit '/static_pages/help'
+     page.should have_selector('title',
+                          :text => 'Ruby on Rails Tutorial Sample App | Help')
+    end
   end
 
   describe "About Us" do
@@ -21,5 +36,11 @@ describe "StaticPages" do
       visit "/static_pages/about"
       page.should have_content('About Us')
   	end	
+
+    it "should have title 'About Us'" do
+      visit "/static_pages/about"
+      page.should have_selector('title',
+                      :text =>'Ruby on Rails Tutorial Sample App | About')
+    end
   end	
 end
