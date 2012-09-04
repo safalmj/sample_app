53c974c058b94e4df527d4e1410656bf59ab91b1
Safal M Joshi <safal@sprout-technology.com>
2012-09-04 16:41:07 +0545

    Added user name and email presence validation and model rspec test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,5 +12,21 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+	before { @user = User.new(:name => "Safal", :email => "safal@gmail.com") }
+
+	subject { @user }
+	it { should respond_to(:name)}
+	it { should respond_to(:email)} 
+
+	it { should be_valid}
+
+	describe "when user is not present" do 
+		before { @user.name = " "}
+		it { should_not be_valid }
+	end
+
+	describe "when email is not present" do 
+		before { @user.email = " "}
+		it { should_not be_valid }
+    end
 end
